05f59dc0e8704aa6c57aedaff1e3bb8a1a4904f2
Barys Yakavita <borisbsu@gmail.com>
2020-09-04 00:52:56 +0300

    Commit

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,19 @@ import { Scanner } from "./Scanner";
 
 const analysisCompleted = () => {
   core.debug('Analysis completed');
-}
+};
 
 async function run(): Promise<void> {
   try {
     core.debug('Run CodeScan analysis');
     new Scanner().runAnalysis(core.getInput('codeScanUrl'), core.getInput('login'), analysisCompleted);
 
-    const ms: string = '500';
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // const ms: string = '500';
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    //
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
@@ -24,4 +24,4 @@ async function run(): Promise<void> {
   }
 }
 
-run()
+run();
