faabe468d47e23fe20481fca94c29e87e0525aac
Barys Yakavita <borisbsu@gmail.com>
2020-09-04 00:55:54 +0300

    Commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 import { Scanner } from "./Scanner";
 
 const analysisCompleted = () => {
@@ -10,13 +9,6 @@ async function run(): Promise<void> {
   try {
     core.debug('Run CodeScan analysis');
     new Scanner().runAnalysis(core.getInput('codeScanUrl'), core.getInput('login'), analysisCompleted);
-
-    // const ms: string = '500';
-    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    //
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
